060ac916cb6492d3c3c1b5cd5baf8a5684cb0d3b
Geraldo Quende <adolfoquende@hotmail.com>
2021-04-04 23:16:34 +0100

    Base form

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Salamandra',
+      debugShowCheckedModeBanner: false,
+      home: Text("Hi"),
+    );
+  }
+}
